55cccdcaa060ddb32cc36ca45619be42b1da9d23
okazaki00 <63778622+okazaki00@users.noreply.github.com>
2022-02-12 13:12:06 +0900

    かりじっそう

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,48 @@
+import 'package:dice/dice.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-void main() {
-  runApp(const MyApp());
-}
+Future<void> main() async => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'CoC dice'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+// 1. StatefulWidgetを継承したクラスを作る。
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
 
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+  // createState()　で"State"（Stateを継承したクラス）を返す
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
+//  2.Stateを継承したクラスを作る。
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _roll1D100() {
     setState(() {
-      _counter++;
+      var rand = math.Random();
+      _counter = rand.nextInt(100) + 1;
     });
   }
 
+  //　状態を使いつつ組んだWidgetを返す(build関数)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,24 +52,90 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            ListTile(
-              title: Text('1D100'),
+          children: [
+            Row(
+              children: [
+                Card(
+                    elevation: 10,
+                    color: Colors.lightGreen,
+                    margin: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(padding: EdgeInsets.only(top: 23, right: 18),
+                          child: Text(
+                            "1D100",
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.only(top: 18, right: 18),
+                          child: Text(
+                            '$_counter',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.only(top: 23, right: 18),
+                          child: TextButton(
+                              onPressed: _roll1D100, child: const Text("roll")
+                          ),),
+                      ],
+                    )
+                ),
+                Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(padding: EdgeInsets.only(top: 23, right: 18),
+                          child: Text(
+                            "1D100",
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.only(top: 18, right: 18),
+                          child: Text(
+                            '$_counter',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.only(top: 23, right: 18),
+                          child: TextButton(
+                              onPressed: _roll1D100, child: const Text("roll")
+                          ),),
+                      ],
+                    )
+                ),
+              ],
             ),
-            ListTile(
-              title: Text('1D8'),
-            ),
-            ListTile(
-              title: Text('1D10'),
-            ),
+            // Row(
+            //   children: [
+            //     const Padding(padding: EdgeInsets.only(top: 23, right: 18),
+            //       child: Text(
+            //           "1D100",
+            //         style: TextStyle(
+            //           fontSize: 24,
+            //         ),
+            //       ),
+            //     ),
+            //     Padding(padding: const EdgeInsets.only(top: 18, right: 18),
+            //       child: Text(
+            //         '$_counter',
+            //         style: Theme.of(context).textTheme.headline4,
+            //       ),
+            //     ),
+            //     Padding(padding: const EdgeInsets.only(top: 23, right: 18),
+            //       child: TextButton(
+            //           onPressed: _roll1D100, child: const Text("roll")
+            //       ),
+            //     ),
+            //   ],
+            // )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
